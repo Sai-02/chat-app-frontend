77039f1e5a17e0977289297a2003e83330066065
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-01-04 23:53:28 +0530

    Header design added

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,8 +3,12 @@ import Header from "../Header/Header";
 
 const Dashboard = () => {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen flex flex-col">
       <Header />
+      <div className="flex-grow grid grid-cols-10">
+        <div className="col-span-3 shadow">e</div>
+        <div className="col-span-7">e</div>
+      </div>
     </div>
   );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { faUserCircle, faBell } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="text-white p-4 px-12 bg-[#1976D2] flex ">
+      <div className="">
+        <h2 className="font-bold text-xl">Chat app</h2>
+      </div>
+      <div className="flex-grow flex flex-row-reverse gap-8">
+        <div className="text-xl cursor-pointer">
+          <FontAwesomeIcon icon={faUserCircle} />
+        </div>
+        <div className="text-xl cursor-pointer">
+          <FontAwesomeIcon icon={faBell} />
+        </div>
+        <div className="text-lg hover:text-[#F9F6EE] cursor-pointer">
+          Create Group
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
