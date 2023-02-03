10c7dc28a146b20cc51c26bac71eb6a7d8c249ff
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-02-03 15:19:45 +0530

    message alignmment fix

--- src/components/ChatWindow/Message.tsx
+++ src/components/ChatWindow/Message.tsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { useAppSelector } from "../../shared/redux/hooks";
 
 interface IMessageComponentProps {
   messageInfo: any;
 }
 const Message = ({ messageInfo }: IMessageComponentProps) => {
+  const user: any = useAppSelector((state) => state.auth.user);
+  const isMessageSentByUser = () =>
+    user?.username === messageInfo?.sender?.username;
   return (
     <div className={`text-black grid `}>
       <div
+        className={`${
+          isMessageSentByUser() ? "justify-self-end hidden" : "justify-self-start"
+        }`}
+      >
+        {messageInfo?.sender?.name}
+      </div>
+      <div
         className={`rounded-full  bg-[#6CB4EE] py-2 px-4 max-w-[60%] shadow ${
-          2 % 2 == 0 ? "justify-self-start" : "justify-self-end"
+          isMessageSentByUser() ? "justify-self-end" : "justify-self-start"
         }`}
       >
         {messageInfo?.message?.text}
