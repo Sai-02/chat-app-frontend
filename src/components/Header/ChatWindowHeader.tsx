5f55b75e830990b24be440d5ec8ecc3dd7341d97
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-03-22 23:36:24 +0530

    [bugfix] : show parsed chat name in case of personal chat

--- src/components/Header/ChatWindowHeader.tsx
+++ src/components/Header/ChatWindowHeader.tsx
@@ -7,8 +7,10 @@ import React, { useEffect, useState } from "react";
 import { useAppSelector } from "../../shared/redux/hooks";
 
 const ChatWindowHeader = () => {
+  const user = useAppSelector((state) => state.auth.user);
   const [groupName, setGroupName] = useState("");
   const [groupMembers, setGroupMembers] = useState("");
+  const [activeChat, setActiveChat] = useState<any>({});
   const isNoChatSelected = () => {
     return activeChatID.trim() === "";
   };
@@ -24,8 +26,14 @@ const ChatWindowHeader = () => {
     const [activeChat, ...x] = chatList.filter(
       (chat) => chat._id === activeChatID
     );
+    setActiveChat(activeChat);
     setGroupName(activeChat.name);
     setGroupMembers(activeChat.members.join(", "));
+  };
+  const getParsedName = (name: any) => {
+    const arr = name.split("+");
+    if (arr[0] === user.username) return arr[1];
+    return arr[0];
   };
   return (
     <div className="">
@@ -36,8 +44,16 @@ const ChatWindowHeader = () => {
           <div className="flex gap-4 items-center">
             <div className="w-10 h-10 rounded-full bg-sky-100"></div>
             <div className="text-left">
-              <h3 className="text-sm font-semibold">{groupName}</h3>
-              <p className="text-xs truncate">{groupMembers}</p>
+              <h3 className="text-sm font-semibold">
+                {activeChat.isGroup ? (
+                  <>{groupName}</>
+                ) : (
+                  <>{getParsedName(groupName)}</>
+                )}
+              </h3>
+              <p className="text-xs truncate">
+                {activeChat.isGroup ? <>{groupMembers}</> : <></>}
+              </p>
             </div>
           </div>
           <div className="flex gap-6 items-center">
